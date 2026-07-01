30af7c27b48c49380c6bfd4eb8e902692fe01557
Neev Chovatiya <neevchovatiya23@gmail.com>
2026-07-01 14:04:50 +0000

    feat: built core database schema for MFR and BMR

---
+++ smart_bmr/smartbmr/doctype/batch_manufacturing_record/batch_manufacturing_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Neev Chovatiya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Batch Manufacturing Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ smart_bmr/smartbmr/doctype/master_formula_record/master_formula_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Neev Chovatiya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Master Formula Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
